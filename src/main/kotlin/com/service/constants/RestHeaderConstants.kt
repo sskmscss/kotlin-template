d56826b49c7d54973eac6ec1541288459950659e
Pragatheeswaran <130035421+PragatheeswaranIdeas2it@users.noreply.github.com>
2023-12-25 21:14:08 +0530

    Update RestHeaderConstants.kt
    Update RestHeaderConstants.kt

--- src/main/kotlin/com/service/constants/RestHeaderConstants.kt
+++ src/main/kotlin/com/service/constants/RestHeaderConstants.kt
@@ -4,6 +4,7 @@ object RestHeaderConstants {
 
     const val TRANSACTION_ID_HEADER = "x-transaction-id"
     const val API_KEY = "API-Key"
+    const val API_KEY = "API-Key"
     const val UM_PUBLISH_TOPIC = "um-publisher-topic"
     const val SOURCE = "source"
     const val SOURCE_ID = "sourceId"
