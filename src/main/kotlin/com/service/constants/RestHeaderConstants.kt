bb5c7ed37532d7f69aca6379b17e1cba28c79143
Pragatheeswaran <130035421+PragatheeswaranIdeas2it@users.noreply.github.com>
2023-12-26 10:28:36 +0530

    Update RestHeaderConstants.kt

--- src/main/kotlin/com/service/constants/RestHeaderConstants.kt
+++ src/main/kotlin/com/service/constants/RestHeaderConstants.kt
@@ -10,3 +10,19 @@ object RestHeaderConstants {
     const val SOURCE_ID = "sourceId"
     const val CONTINUATION_TOKEN = "continuationToken"
 }
+object ExceptionMessage {
+
+    const val NO_DATA_FOUND = "No Data Found"
+    const val NO_OFFER_FOUND = "No valid offer is present identified by Id"
+    const val BAD_PARAMETERS = "Bad Parameters"
+    const val BAD_REQUEST = "Bad Request"
+    const val MULTI_STATUS_PUT = "Updated with errors and/or warnings"
+    const val MULTI_STATUS_POST = "Created with errors and/or warnings"
+    const val INTERNAL_ERROR = "Internal Error"
+    const val FORBIDDEN = "Forbidden"
+    const val ACCEPTED = "Accepted"
+    const val OK = "OK"
+    const val CREATED = "Created"
+    const val API_KEY_NOT_VALID = "API KEY is not valid"
+    const val API_KEY_NOT_PROVIDED = "API KEY is not provided"
+}
